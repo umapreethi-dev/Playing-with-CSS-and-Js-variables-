ff808dab579a393d08a98468d15ae255114a377a
umapreethi <umapreethi123.up@gmail.com>
2021-09-18 19:55:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const inputs = document.querySelectorAll(' .controls input');
+console.log(inputs);
+
+function handleUpdate(){
+    const suffix = this.dataset.sizing || '';
+    console.log(this.value);
+    console.log(suffix);
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
